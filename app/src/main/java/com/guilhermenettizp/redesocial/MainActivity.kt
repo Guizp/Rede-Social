90b1ab3cb33a819f5d0277dcf357402db5546cd6
Guilherme Gilon Simonetti <guilherme.simonetti@outlook.com>
2026-03-27 17:50:53 -0300

    first commit

---
+++ app/src/main/java/com/guilhermenettizp/redesocial/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.guilhermenettizp.redesocial
+import android.app.Activity
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.ImageRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.guilhermenettizp.redesocial.databinding.ActivityMainBinding
+
+class MainActivity : Activity()
+{
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val queue: RequestQueue = Volley.newRequestQueue(this)
+        val urlTexto = "http://10.105.68.80:8080/exemplo/texto"
+        val urlImage = "http://10.105.68.80:8080/exemplo/imagem"
+
+        val stringRequest = StringRequest(Request.Method.GET, urlTexto,
+            { response ->
+                binding.txtPost.text = response
+            },
+            { error ->
+                error.printStackTrace()
+            }
+        )
+
+        val imageRequest = ImageRequest(urlImage,
+            { response ->
+                binding.imagePost.setImageBitmap(response)
+            },
+            0, 0, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.RGB_565,
+            { error ->
+                error.printStackTrace()
+            }
+        )
+        queue.add(stringRequest)
+        queue.add(imageRequest)
+        queue.start()
+    }
+}
